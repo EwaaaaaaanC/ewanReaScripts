2ddd27b419f74bafc19770317e45e4b8f66b20a9
EwaaaaaaanC <ewanc@grindinggear.com>
2026-05-29 12:06:45 +1200

    Added new script: paste from NPCTalkDialogueTextAudio
    Allows fast setup for scratch tracks

---
+++ ewan - Utilities/ewan - Paste NPCTalkDialogueTextAudio from Config Editor.lua
@@ -0,0 +1,132 @@
+-- @description Paste NPCTalkDialogueTextAudio from Config Editor
+-- @author ewan
+-- @version 0.9
+-- @about
+--   Copy the first five columns from config and paste them into reaper.
+
+--HOW TO:
+-- COPY the first FIVE columns in config for the steps you want to paste.
+-- Then, run this action at the point in the reaper session you wish to insert them.
+-- Enter the number of seconds you want to separate each line.
+
+dialoguePath = [[D:\SVN\4.0.0\bin\Client\Audio\Dialogue\]]
+-- Paste your dialogue path above, if not the same as mine. 
+retval, spacing = reaper.GetUserInputs("ewan: Paste From Config (NPCTalkDialogueTextAudio)", 1, "Space between lines(s)","5")
+
+clipboard = reaper.CF_GetClipboard('')
+
+configTable = {}
+-- Iterate through each line and insert it into the table
+for line in clipboard:gmatch("[^\r\n\t]+") do
+    table.insert(configTable, line)
+end
+
+function reduceToFileNameOnly(input)
+local extensionRemoved = (string.gsub(input, ".ogg", ""))
+local fileNameOnly = string.match(extensionRemoved, "([^/]+)$")
+return fileNameOnly
+end
+
+function pasteConfigAudio (path,textinput)
+local pos = reaper.GetCursorPosition()
+local fullpath = dialoguePath..path
+itemStartPos = reaper.GetCursorPosition()
+local fixed_path = (string.gsub(fullpath, "\\\\", "/"))
+reaper.InsertMedia(fixed_path, 0)
+local itemEndPos = reaper.GetCursorPosition()
+reaper.SetEditCurPos(itemEndPos+spacing,true,false)
+
+local fileName = reduceToFileNameOnly(path)
+reaper.AddProjectMarker2(0, false, itemStartPos, 0, fileName, -1, 0)
+--adds a marker with filename for rendering.
+
+
+local track = reaper.GetSelectedTrack(0, 0)
+local item = reaper.AddMediaItemToTrack(track)
+reaper.SetMediaItemInfo_Value(item, "D_POSITION", pos)
+reaper.SetMediaItemInfo_Value(item, "D_LENGTH", itemEndPos-pos+spacing-2) -- Length in seconds
+reaper.GetSetMediaItemInfo_String(item,"P_NOTES",textinput,true)
+
+
+
+end
+
+function main()
+
+id = ""
+speaker = ""
+text = ""
+assetList = {}
+textList = {}
+
+stepCount = #configTable/4
+
+for i = 1, stepCount do
+  assetList = {}
+  textList = {}
+  id = configTable[i*4-3]
+  speaker = configTable[i*4-2]
+  text = configTable[i*4-1]
+  assets = configTable[i*4]
+--above gets data for each step.
+
+reaper.SelectAllMediaItems(0,false)
+--deselectallitems
+
+assetsFormatted = string.gsub(assets, ", ", "\n")
+for line in assetsFormatted:gmatch("[^\r\n]+") do
+    table.insert(assetList, line)
+end
+-- above four lines formats assets for inserting.
+
+textSplit = string.gsub(text, "<continue>", "\n")
+for line in textSplit:gmatch("[^\r\n]+") do
+    table.insert(textList, line)
+end
+-- above four lines formats text for inserting.
+
+currentPos = reaper.GetCursorPosition()
+
+for i = 1, #assetList do
+pasteAsset = assetList[i]
+pasteText = textList[i]
+pasteConfigAudio(pasteAsset,pasteText)
+end
+--above pastes assets in a row.
+
+endPos = reaper.GetCursorPosition()
+--reaper.SetEditCurPos(endPos+30,true,false)
+--above moves the cursor forwards before next assets are pasted.
+
+--insert an item below with the text on it
+
+reaper.AddProjectMarker2(0, true, currentPos, endPos-1, id, -1, 0)
+--insert region with id
+
+end
+
+end
+
+reaper.Undo_BeginBlock()
+
+main()
+
+reaper.Undo_EndBlock("Paste from Config Editor",-1)
+
+
+
+
+ 
+
+
+
+
+
+
+
+--for i = 1, #configTable do
+--pasteTarget = configTable[i]
+--pasteConfigAudio(pasteTarget)
+--end
+
+
